435386c97b490a08da0a339d250c1c0139c4e9a5
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2024-10-07 20:35:14 +0300

    Search with server side fetch and keyd Suspense

--- src/app/loading.tsx
+++
@@ -1,5 +0,0 @@
-import LoadingSpinner from "@/components/common/LoadingSpinner";
-
-export default function loading() {
-  return <LoadingSpinner className="w-16 h-16 mx-auto" />;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
+import LoadingSpinner from "@/components/common/LoadingSpinner";
 import BrandFilter from "@/components/search/BrandFilter";
 import SearchForm from "@/components/search/SearchForm";
 import SearchProvider from "@/components/search/SearchProvider";
 import SearchResults from "@/components/search/SearchResults";
 import { search } from "@/data/search";
+import { Suspense } from "react";
 
 export const dynamic = "force-dynamic";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: unknown;
-}) {
-  const searchResult = await search(searchParams);
-
+export default function Home({ searchParams }: { searchParams: unknown }) {
   return (
-    <SearchProvider searchResult={searchResult}>
+    <>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Running Shoes Search
       </h1>
@@ -23,6 +19,20 @@ export default async function Home({
         filter the search results by the shoe brand.
       </p>
       <SearchForm />
+      <Suspense
+        key={JSON.stringify(searchParams)}
+        fallback={<LoadingSpinner className="w-16 h-16 mx-auto" />}
+      >
+        <DataProvider searchParams={searchParams} />
+      </Suspense>
+    </>
+  );
+}
+
+async function DataProvider({ searchParams }: { searchParams: unknown }) {
+  const searchResult = await search(searchParams);
+  return (
+    <SearchProvider searchResult={searchResult}>
       <BrandFilter />
       <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
         Search Results
